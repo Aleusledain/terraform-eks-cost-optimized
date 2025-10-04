c14570bc906685bc8e636b705afc17a29b031253
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-10-04 05:34:24 -0400

    main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "smartapp-vpc"
+  }
+}
+
+# Create public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create private subnet
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Create an EKS cluster
+resource "aws_eks_cluster" "smart_cluster" {
+  name     = "smartapp-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public.id,
+      aws_subnet.private.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
+}
+
+# IAM Role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eksClusterRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
